511a6c453a1c363629e40743cc26144643aba9ce
Utin Chindy <utincindyselvira16@gmail.com>
2020-12-11 13:46:39 +0700

    Initial commit

---
+++ app/src/main/java/com/utinchindyselvira/leptop/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.utinchindyselvira.leptop
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
